cba4c026cf66a7f81605cf6a617b76056ae31d84
chenguanlin <chenguanlin@LX.com>
2024-01-22 10:19:56 +0800

    annotation

--- jbase/src/main/java/annotation/AnnoTest.java
+++ jbase/src/main/java/annotation/AnnoTest.java
@@ -1,7 +1,24 @@
 package annotation;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+
+@Data
+@Slf4j
 public class AnnoTest {
     public static void main(String[] args) {
 
+        Class cls = AnnoTest.class;
+        Annotation[] annotations = cls.getAnnotations();
+        Annotation[] declaredAnnotations = cls.getDeclaredAnnotations();
+//        Data annotation = cls.getAnnotation(Data.class);
+//        boolean present = cls.isAnnotationPresent(Slf4j.class);
+        log.info("annotations:{}", Arrays.toString(annotations));
+        log.info("declaredAnnotations:{}",Arrays.toString(declaredAnnotations));
+//        log.info("annotation:{}",annotation);
+//        log.info("present:{}",present);
     }
 }
