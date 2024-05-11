08fa41c8f5dc287cad0e39de529130887f718e3b
chenguanlin <chenguanlin@LX.com>
2024-05-11 11:15:57 +0800

    gzip

--- jbase/src/main/java/io/IoTest.java
+++ jbase/src/main/java/io/IoTest.java
@@ -1,5 +1,29 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
 public class IoTest {
 
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static void readGzFile(String gzFile) throws IOException {
+        try (
+                FileInputStream fis = new FileInputStream(gzFile);
+                GZIPInputStream gzipIn = new GZIPInputStream(fis);
+                InputStreamReader isr = new InputStreamReader(gzipIn);
+                BufferedReader reader = new BufferedReader(isr);
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
 }
